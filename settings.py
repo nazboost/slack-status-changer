c4780cb1d84e07a5c0d2b91bb2bf6a81c02e4aa3
Nazna <naz6oost@gmail.com>
2019-03-04 15:01:05 +0900

    Update .env

--- settings.py
+++ settings.py
@@ -6,5 +6,4 @@ load_dotenv(dotenv_path=dotenv_path)
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
-verification_token = os.environ.get('verification_token')
 token = os.environ.get('token')
